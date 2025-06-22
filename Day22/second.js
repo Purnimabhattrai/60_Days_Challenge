a405f2374a120d7ba2234968dd5607fb37e37d39
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-06-22 22:40:17 +0545

    operators

--- Day22/second.js
+++ Day22/second.js
@@ -1,1 +1,20 @@
- 
+   //unary operator
+//  let a=5;
+//  let b=2;
+//  console.log("a=",a,"&b=",b);
+//  a--;
+//  console.log("a=",a);
+
+  //comparision operator//
+//  let a=6;
+//  let b=5;
+//  console.log("6>=5",a>=b);
+
+   //logical operators//
+       let a=4;
+       let b=8;
+       let cond1= a<b; //true
+       let cond2= a===b; //false
+       console.log("cond1 && cond2=", cond1 && cond2);
+
+
