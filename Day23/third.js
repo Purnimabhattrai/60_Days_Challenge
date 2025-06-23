d29a12210f58b6554556394a4ab2dd2db47a69dc
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-06-23 19:21:03 +0545

    Day23

--- Day23/third.js
+++ Day23/third.js
@@ -1,4 +1,4 @@
- console.log("Hello World");
+ console.log("Hello World!!!");
 
 ///conditional statements///
 //    const age =2;
@@ -34,3 +34,37 @@
 //     console.log('odd');
 // }
 
+//loops
+
+//for loop-> if you know how many times you have to run the loop
+// for(let i=1;i<=10; i=i+1){
+//     console.log("Purnima" +i)
+// }
+
+//while loop->if you know the condition and task but dont know how many time it have to run to achieve that task
+
+// let ip=50
+// let house=50
+
+// while(ip !=house){
+//     ip=ip+1;
+//     console.log('step taken'+ip)
+// }
+
+//do while->deko condition true nahuda samma run garxa. pahila code run garxa aani condition check garxa
+//   do{
+//        ip=ip+1;
+//        console.log('step taken'+ip)
+//   }while(ip<=house)
+
+/////////guees the input game///////////////
+let number=9;
+
+let guess= 0;
+do  {
+    guess =parseInt(prompt("Guess a number"))
+    if(guess == number){
+        alert("Winner")
+        break;
+    }
+}while (guess !=0)
