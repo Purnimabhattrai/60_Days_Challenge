fb12d7bb3a91805fb197ffae5d1675ca07953888
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-06-23 19:00:37 +0545

    Logical operators

--- Day23/third.js
+++ Day23/third.js
@@ -12,3 +12,25 @@
 //  const age=22;
 //  age>=18? console.log('yes'): console.log('no');
 
+//switch case//
+// const option=8;
+// switch(option){
+//     case 1:console.log("Namaste")
+//     break;
+//     case 2:console.log("hi");
+//     break;
+//     case 3:console.log("bye!!!")
+//     break;
+//     default:console.log("invalid option");
+// }
+
+///logical operators//
+
+// const number=5;
+// if(number%2==0){
+// //     console.log('even');
+// // }
+// if(number% 2!=0) {
+//     console.log('odd');
+// }
+
