88725bdb3c5b413b94ee335d4a96c20daf23cfd2
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-06-23 18:44:25 +0545

    ConditionalStatements

---
+++ Day23/third.js
@@ -0,0 +1,14 @@
+ console.log("Hello World");
+
+///conditional statements///
+//    const age =2;
+//    if(age>=18){
+//       console.log ('yes! you can vote');
+//     }else{
+//         console.log ('No! you cant vote');
+//      }
+
+ //ternary operator///
+//  const age=22;
+//  age>=18? console.log('yes'): console.log('no');
+
