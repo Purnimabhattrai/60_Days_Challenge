b4506df45d22e31c69ad59aa80458a023849d48a
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-06-24 19:36:30 +0545

    funstions created

---
+++ Day24/fourth.js
@@ -0,0 +1,34 @@
+ ///funstions->  block of code that perform certain task///
+
+ function sayHello(){
+    console.log("hey Purnima")
+ }
+ sayHello()  // calling of function///
+
+ ////parametarized funstions
+ function add(a,b){
+    console.log(a+b);
+ }
+ add(5,2)
+ add(10,10)
+ add(9,0)
+
+ function mul(a,b){
+    // console.log(a*b);  //not a good practice
+    return a*b;   
+ }
+  let a= mul(2,3);
+  console.log('this is the result',a)
+
+//unlimited arguments accept
+function addNumbers(){
+    let ans =0
+    for(let i=0;i<arguments .length;i=i+1){
+        ans= ans+arguments[i]
+    }
+    return ans
+}
+let result= addNumbers(10,20,4,40,50)
+console.log(result)
+
+   
