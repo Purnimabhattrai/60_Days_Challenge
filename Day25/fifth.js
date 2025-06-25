a9587f4b9ca9a6bd4c097a6f015f7fc71f64b901
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-06-25 22:06:55 +0545

    traditional vs arrow

--- Day25/fifth.js
+++ Day25/fifth.js
@@ -1,7 +1,13 @@
  // Traditional function
 const add = function(a, b) {
   return a + b;
-};
+  }
+  let a=add(2,3);
+  console.log("the answwer is",a);
+
 
 // // Arrow function
-const mul = (a, b) => a + b;
+const mul = (a, b) => a * b;
+let b=mul(2,3);
+console.log("the answer is",b);
+
