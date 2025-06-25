a9b43678daf7b9ae2eecfb84bbf99b6091b4e736
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-06-25 21:58:49 +0545

    arrowFunc

--- Day24/fourth.js
+++ Day24/fourth.js
@@ -1,42 +1,44 @@
- ///funstions->  block of code that perform certain task///
+ ///function->  block of code that perform certain task///
 
  function sayHello(){
-    console.log("hey Purnima")
+    console.log("hey Purnima");
  }
- sayHello()  // calling of function///
+ sayHello();  // calling of function///
 
  ////parametarized funstions
  function add(a,b){
     console.log(a+b);
  }
- add(5,2)
- add(10,10)
- add(9,0)
+ add(5,2);
+ add(10,10);
+ add(9,0);
 
  function mul(a,b){
     // console.log(a*b);  //not a good practice
     return a*b;   
  }
   let a= mul(2,3);
-  console.log('this is the result',a)
+  console.log('this is the result',a);
 
 //unlimited arguments accept
 function addNumbers(){
     let ans =0
     for(let i=0;i<arguments .length;i=i+1){
-        ans= ans+arguments[i]
+        ans= ans+arguments[i];
     }
     return ans
 }
-let result= addNumbers(10,20,4,40,50)
-console.log(result)
+let result= addNumbers(10,20,4,40,50);
+console.log(result);
 
 //arrow functions
 //syntax:- const sayHelllo =()=> a+b{
 //     console.log("hey"); or 
 // return a+b;
 // }
-//arguments:- const addNumbers =(...nums)=>{ ///[.....nums.. is arrow function]
+
+
+//arguments:- const addNumbers =(...nums)=>{        [.....nums.. is arrow function]
     // console.log(nums);}
     //add numbers(10,20,20,30);
 
---
+++ Day25/fifth.js
@@ -0,0 +1,7 @@
+ // Traditional function
+const add = function(a, b) {
+  return a + b;
+};
+
+// // Arrow function
+const mul = (a, b) => a + b;
