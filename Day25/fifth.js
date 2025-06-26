4d9e8929f337c9701c5e8d892bbf452970ee154e
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-06-26 21:00:27 +0545

    Day26

--- Day25/fifth.js
+++ Day25/fifth.js
@@ -3,11 +3,14 @@ const add = function(a, b) {
   return a + b;
   }
   let a=add(2,3);
-  console.log("the answwer is",a);
+  console.log("the answer is",a);
 
 
 // // Arrow function
 const mul = (a, b) => a * b;
 let b=mul(2,3);
 console.log("the answer is",b);
+
+
+
 
