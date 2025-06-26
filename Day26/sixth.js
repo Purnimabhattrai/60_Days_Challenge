92095d48f8f03760d1c8765f3a45ab2b00f65ccd
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-06-26 21:28:13 +0545

    HigherOrderFunction

--- Day26/sixth.js
+++ Day26/sixth.js
@@ -1,1 +1,9 @@
- 
+ ///higherorder function and callback(cb)
+ //a function that takes function as arguments//
+
+function add(a,b,cb){
+    let result=a+b;
+    cb(result);
+}
+add(2,4,(val) => console.log(val));
+add(400,400,(val)=>console.log(val));
