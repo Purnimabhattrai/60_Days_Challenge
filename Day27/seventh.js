a397476ac25369b80b6b21ce6f8fa62e5982d98b
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-06-30 21:43:19 +0545

    DOM Day29

--- Day27/seventh.js
+++ Day27/seventh.js
@@ -2,11 +2,17 @@
  //used when we have to store multiple values in one variable//
 
  const students=["purnima","prejibsa","jyoti","basanta"];
- console.log(students.length);///students garda list aauxa sabbai ko and student.length garda kati ota xan value aauxa///
- students[0]="Purnima Bhattrai"; //we can change the value of the specified index in array//
- console.log(students[0]); //[0]index value dida tyo index ma jun item xa tesko details dinxa///
+ console.log(students);
+ ///students garda list aauxa sabbai ko and student.length garda kati ota xan value aauxa///
 
- ///hamley  student lai const declare garye pani value change payeraxam but only the existing value of the array ,we cant add any new value rather than the already present value//
+ students[0]="Purnima Bhattrai";
+  //we can change the value of the specified index in array//
+
+   console.log(students[0]);
+  //[0]index value dida tyo index ma jun item xa tesko details dinxa///
+
+ ///hamley  student lai const declare garye pani value change payeraxam but only the existing value of the array, 
+ // we can't add any new value rather than the already present value//
 //ie: array ko index j xa tehi hunxa tesma value change garna sakinxa//
 
 
@@ -14,6 +20,13 @@
 students.push("lila","laxmi","arpan");
 console.log(students);
 
+//Adds the item to begining//
+students.unishift("sanjog");
+
+//removes the item from begining//
+ students.shift();
+
+ 
 ///DELETING ARRAY FROM ARRAY///
 students.pop();
 console.log(students);
