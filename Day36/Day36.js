037de1b0f574fa7b689f8c4e29118a7295f6e3f7
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-07-06 21:15:29 +0545

    Color Selector

--- Day36/Day36.js
+++ Day36/Day36.js
@@ -1,1 +1,10 @@
- 
+// Select all color boxes
+const colorBoxes = document.querySelectorAll(".color-box");
+
+// Add click event listener to each box
+colorBoxes.forEach(box => {
+  box.addEventListener("click", () => {
+    const color = box.getAttribute("data-color");
+    document.body.style.backgroundColor = color;
+  });
+});
