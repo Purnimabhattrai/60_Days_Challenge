e1a9e063d2128da24f7c8e8455c0bd0755e25983
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-07-13 16:42:56 +0545

    optimized code

--- Day42/project.js
+++ Day42/project.js
@@ -20,6 +20,7 @@ const startChangingColor=function(){
 
 const stopChangingColor=function(){
     clearInterval(intervalId);
+    intervalId=  null;
 }
 
 document.querySelector('#start').addEventListener
