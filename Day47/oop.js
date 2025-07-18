75309bc806d0da7c0819c76b6b92ecf5c83adf01
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-07-18 20:57:15 +0545

    Day48

--- Day47/oop.js
+++ Day47/oop.js
@@ -1,11 +1,32 @@
- const user={
-    username:"purnima",
-    loginCount:9,
-    signedIn:true,
-    getUserDetails:function(){
-      //  console.log('username:${this.username}');
-        console.log(this);
-    }
+//  const user={
+//     username:"purnima",
+//     loginCount:9,
+//     signedIn:true,
+//     getUserDetails:function(){
+//        console.log('username:${this.username}');
+//         console.log(this);
+//     }
+//  }
+//  console.log(user.username)
+//  console.log(this);
+
+
+ function User(username,loginCount,isLoggedIn){
+   this.username= username;
+   this.loginCount=loginCount;
+   this.isLoggedIn=isLoggedIn;
+   return this;
  }
- //console.log(user.username)
- console.log(this);
+//  const userOne=User("Purnima",29,true)
+//  const userTwo=User("Prejibsa",23,false)  this override the user one
+//  console.log(userOne);
+
+const userOne= new User("Purnima",29,true)
+const userTwo= new User("Prejibsa",23,false)  
+console.log(userOne);
+console.log(userTwo);
+
+//new use garda new obj create hunxa
+//constructor function call hunxa
+//aruments inject hunxa 
+//value aauxa
