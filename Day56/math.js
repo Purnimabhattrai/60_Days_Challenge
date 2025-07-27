9386b21fe46ec0a11d7d96b91cdf5b2a35446d51
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-07-27 20:29:42 +0545

    propertydefine

--- Day56/math.js
+++ Day56/math.js
@@ -10,5 +10,20 @@ const chai={
     name:'ginger chai',
     price:250,
     isAvailable:true,
+    orderChai: function(){
+        console.log("chai drink");
+    }
 }
 console.log(Object.getOwnPropertyDescriptor(chai,"name"))
+Object.defineProperty(chai,'name',{
+    enumerable:true,
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+for (let[key,value] of Object.entries(chai)){
+    if(typeof value!=='function'){
+        console.log(`${key}:${value}`);
+    }
+}
+
