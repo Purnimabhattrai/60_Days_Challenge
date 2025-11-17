c0d88656e1d2d2a7eebc3d1a7017a9f76ebed85a
Purnima Bhattrai <purnimabhattrai.72@gmail.com>
2025-11-17 23:28:47 +0545

    data conversion

--- Day60/learning.js
+++ Day60/learning.js
@@ -1,9 +1,14 @@
- let name ="purnima";
- let age =18;
- let state;
- console.log(typeof purnima);
-    console.log(typeof age);
+//  let name ="purnima";
+//  let age =18;
+//  let state;
+//  console.log(typeof purnima);
+//     console.log(typeof age);
     
 
+let score = 29;
+console.log(typeof score);
 
-
+let str1= "hi"
+let str2= " purnima"
+let str3= str1 + str2
+console.log(str3)
